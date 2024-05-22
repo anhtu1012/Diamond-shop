d2ab5c2b05be83b18483c31cce3119c11a6957fa
phamanhtu1012@gmail.com <phamanhtu1012@gmail.com>
2024-05-22 18:31:56 +0700

    uplate layout

--- services/Uservices.js
+++ services/Uservices.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import api from "../src/config/axios";
 export const loginApi = (email, password) => {
-  return axios.post(
+  return api.post(
     "https://diplomatic-energy-production.up.railway.app/swp391/api/user/login",
     {
       email,
