973319cb59a391d444557177bf5a4043942b7b87
haintse170559 <haintse170559@fpt.edu.vn>
2024-05-26 23:37:03 +0700

    #12 fix sơ các lỗi vặt và update lại

--- src/components/Foster/index.jsx
+++ src/components/Foster/index.jsx
@@ -1,39 +1,43 @@
 // src/components/Footer.jsx
-
+import { Link } from 'react-router-dom';
 import './index.scss';
 
-const Footer = () => {
+const Foster = () => {
+  const linkStyle = {
+    color: 'white', 
+    textDecoration: 'none'
+  };
   return (
     <div className="footer-container">
       <div className="footer-content">
-        <div className='logo'>
-        <a href="/">
-            <img 
-              src='https://firebasestorage.googleapis.com/v0/b/diamond-6401b.appspot.com/o/Logo.png?alt=media&token=13f983ed-b3e1-4bbe-83b2-a47edf62c6a6' 
+        <div className='logo' >
+        <Link to="/" >
+            <img  
+              src='https://firebasestorage.googleapis.com/v0/b/diamond-6401b.appspot.com/o/Logo-phong-trang.png?alt=media&token=f7f1e2f9-3b82-40c2-b02c-eeb19d88fb4c' 
               alt='logo' 
             />
-          </a>
+          </Link>
         </div>
-        <div className="footer-section">
-          <h2>VỀ CHÚNG TÔI</h2>
+        <div className="footer-section" >
+          <h2>VỀ CHÚNG TÔI</h2><br/>
           <div className="social-icons">
-            <a href="/1" rel="nofollow" target="_blank">
-              <i className="fab fa-facebook-f"></i> Vì sao chọn Diamond
+            <a href="/1" rel="nofollow" target="_blank" style={linkStyle}>
+               Vì sao chọn Diamond
             </a>
           </div>
         </div>
         <div className="footer-section">
-          <h2>LIÊN HỆ Diamond</h2>
-          <p>Hotline<br />
-            CN HCM: <a href="tel:0123456780" rel="nofollow">012 345 6780</a> (phím 1)<br />
-            GÓP Ý: <a href="tel:0123456890" rel="nofollow">012 345 6780</a> (phím 2)
-          </p>
+          <h2>LIÊN HỆ Diamond</h2><br/>
+          <p>Hotline<br/>
+            CN HCM: <a href="tel:0123456780" rel="nofollow" style={linkStyle}>012 345 6780</a> (phím 1)<br />
+            GÓP Ý: <a href="tel:0123456890" rel="nofollow" style={linkStyle}>012 345 6780</a> (phím 2)
+          </p><br/>
           <p>Hệ Thống Showrooms<br />
-            CN HCM: <a href="https://www.google.com/maps/place/VNUHCM+Student+Cultural+House/@10.8751238,106.7981485,17z/data=!3m1!4b1!4m6!3m5!1s0x3174d8a6b19d6763:0x143c54525028b2e!8m2!3d10.8751238!4d106.8007234!16s%2Fg%2F11hd1pf9gj?entry=ttu" rel="nofollow" target="_blank">Nhà Văn Hóa Sinh Viên</a><br />
+            CN HCM: <a href="https://www.google.com/maps/place/VNUHCM+Student+Cultural+House/@10.8751238,106.7981485,17z/data=!3m1!4b1!4m6!3m5!1s0x3174d8a6b19d6763:0x143c54525028b2e!8m2!3d10.8751238!4d106.8007234!16s%2Fg%2F11hd1pf9gj?entry=ttu" rel="nofollow" target="_blank" style={linkStyle}>Nhà Văn Hóa Sinh Viên</a><br />
           </p>
         </div>
         <div className="footer-section">
-          <h2>CHỨNG NHẬN</h2>
+          <h2>CHỨNG NHẬN</h2><br/>
           <div className="certificates">
             <img src="https://firebasestorage.googleapis.com/v0/b/diamond-6401b.appspot.com/o/Logo%20Bo%20cong%20thuong.png?alt=media&token=c3ce7c08-8d52-4cd6-bb64-5f88585c698b" alt="bocongthuong" />
             <img src="https://firebasestorage.googleapis.com/v0/b/diamond-6401b.appspot.com/o/DMCA.png?alt=media&token=773286c0-7445-4281-a542-113d6c2632af" alt="dmca" />
@@ -48,4 +52,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+export default Foster;
--- src/page/layout/index.jsx
+++ src/page/layout/index.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/header";
+import Foster from "../../components/Foster";
 
 function Layout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Foster/>
     </div>
   );
 }
