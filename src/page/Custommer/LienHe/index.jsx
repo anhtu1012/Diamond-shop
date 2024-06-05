6837a76497f1bab5b7070cffcce888d6837f9e46
haintse170559 <haintse170559@fpt.edu.vn>
2024-06-06 01:32:43 +0700

    #43 trang lien he

--- src/page/Custommer/LienHe/index.jsx
+++ src/page/Custommer/LienHe/index.jsx
@@ -2,7 +2,7 @@ import { Col, Row } from "antd";
 import Container from "../../../components/container/Container";
 import { Link } from "react-router-dom";
 import { AiOutlineCheck } from "react-icons/ai";
-
+import "./index.scss";
 function LienHe() {
   return (
     <div>
@@ -10,7 +10,7 @@ function LienHe() {
         <div className="lien-he">
           <div className="anh">
             <img
-              src="https://jemmia.vn/wp-content/uploads/2022/10/jemmia.jpg"
+              src="https://a-static.besthdwallpaper.com/diamond-jewels-wallpaper-2048x576-11133_71.jpg"
               style={{ width: "100%", height: "auto" }}
             />
           </div>
@@ -23,7 +23,7 @@ function LienHe() {
           >
             Liên Hệ Với Chúng Tôi
           </h1>
-          <div className="thong-tin-lien-he">
+          <div className="thong-tin-lien-he" style={{ marginLeft: "100px" }}>
             <Row>
               <Col span={12}>
                 <div className="thong-tin-lien-he-1">
@@ -87,27 +87,144 @@ function LienHe() {
                   <div className="facebook-lien-he">
                     <p style={{ fontSize: "18px", marginTop: "30px" }}>
                       Facebook:
+                      <span style={{ marginLeft: "10px" }}>
+                        <a href="https://www.facebook.com" rel="nofollow">
+                          Trang chủ Facebook của Diamond King{" "}
+                        </a>
+                      </span>
                     </p>
-                    <p
-                        style={{ marginLeft: "10px", display: "inline-block" }}
-                      >
-                        <a href="tel:0916306945" rel="nofollow">
-                          012 345 6789
-                        </a>{" "}
-                        (phím 2)
-                      </p>
                   </div>
                   <div className="zalo-lien-he">
-                    <p style={{ fontSize: "18px", marginTop: "30px" }}>Zalo:</p>
+                    <p style={{ fontSize: "18px", marginTop: "30px" }}>
+                      Zalo:
+                      <span style={{ marginLeft: "10px" }}>
+                        <a
+                          href="https://id.zalo.me/account?continue=https%3A%2F%2Fchat.zalo.me%2F"
+                          rel="nofollow"
+                        >
+                          Zalo Tư Vấn Của Diamond King{" "}
+                        </a>
+                      </span>
+                    </p>
                   </div>
                   <div className="insta-lien-he">
                     <p style={{ fontSize: "18px", marginTop: "30px" }}>
                       Instagram:
+                      <span style={{ marginLeft: "10px" }}>
+                        <a
+                          href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fterms%2Faccept%2F%3F__coig_login%3D1"
+                          rel="nofollow"
+                        >
+                          Trang chủ Instagram của Diamond King{" "}
+                        </a>
+                      </span>
                     </p>
+                  </div>
+                  <div
+                    className="liên-he-ngay"
+                    style={{ marginBottom: "40px" }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "25px",
+                        marginTop: "30px",
+                        fontWeight: "400",
+                        marginBottom: "30px",
+                      }}
+                    >
+                      Hãy Liên Hệ Chúng Tôi Ngay Để Được Tư Vấn
+                    </p>
+                    <div className="contact-form-wrapper">
+                      <form
+                        className="contact-form"
+                        method="post"
+                        id="contact-form"
+                        name="contact-form"
+                      >
+                        <div className="form-fields-wrapper">
+                          <div className="form-field">
+                            <input
+                              size="1"
+                              type="text"
+                              name="form_fields[name]"
+                              id="form-field-name"
+                              className="text-field"
+                              placeholder="Họ và tên (*)"
+                              required="required"
+                              aria-required="true"
+                              pattern="[a-zA-ZÀ-ÿ\s]{1,}"
+                            />
+                          </div>
+                          <div className="form-field">
+                            <input
+                              size="1"
+                              type="text"
+                              name="form_fields[lastname]"
+                              id="form-field-lastname"
+                              className="text-field"
+                              placeholder="Số điện thoại (*)"
+                              required="required"
+                              aria-required="true"
+                            />
+                          </div>
+                          <div className="form-field">
+                            <input
+                              size="1"
+                              type="email"
+                              name="form_fields[email]"
+                              id="form-field-email"
+                              className="text-field"
+                              placeholder="Email (*)"
+                              required="required"
+                              aria-required="true"
+                            />
+                          </div>
+                          <div className="form-field">
+                            <textarea
+                              name="form_fields[message]"
+                              id="form-field-message"
+                              className="text-area"
+                              placeholder="Tin nhắn"
+                            ></textarea>
+                          </div>
+                          <div>
+                            <button
+                              type="submit"
+                              className="elementor-button elementor-size-sm"
+                              id="Button_From_Lien_He"
+                            >
+                              <span>
+                                <span className=" elementor-button-icon"></span>
+                                <span className="elementor-button-text">
+                                  LIÊN HỆ NGAY
+                                </span>
+                              </span>
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </Col>
-              <Col span={12}>col-12</Col>
+              <Col span={12}>
+                <div className="thong-tin-lien-he-1">
+                  <p style={{ fontSize: "25px", marginBottom: "30px" }}>
+                    Chi Nhánh Hồ Chí Minh
+                  </p>
+                  <div className="google-map">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.1640560267656!2d106.8007234!3d10.875123799999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d8a6b19d6763%3A0x143c54525028b2e!2sVNUHCM%20Student%20Cultural%20House!5e0!3m2!1sen!2s!4v1717609028517!5m2!1sen!2s"
+                      width={400}
+                      height={300}
+                      style={{ border: 0 }}
+                      allowFullScreen
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    />
+                  </div>
+                </div>
+              </Col>
             </Row>
           </div>
         </div>
